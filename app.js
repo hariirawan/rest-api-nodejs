00be78c6752a7d053ad19d28f208972667473233
hariirawan <hari31121993.com>
2018-05-05 16:22:09 +0700

    crud

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const productRoutes = require('./src/routes/products');
 const orderRoutes = require('./src/routes/orders');
+
+mongoose.connect("mongodb://127.0.0.1:27017/rest-api-shop")
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.use((res, res, next) => {
+
+app.use((req, res, next) => {
    res.header('Access-Control-Allow-Origin', '*');
    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
@@ -39,5 +44,6 @@ app.use((err, req, res, next) => {
       }
    })
 })
+mongoose.Promise = global.Promise;
 
 module.exports = app;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const monggose= require('mongoose');
+
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-   res.status(200).json({
-      message:'Handling GET requests to /products'
-   });
+   Product
+      .find()
+      .exec()
+      .then(docs => {
+         res.status(200).json({
+            docs
+         })
+      })
+      .catch(err => {
+         res.status(500).json({
+            message:'error'
+         })
+      })
 });
 
 router.post('/', (req, res, next) => {
-   const product = {
+   const product = new Product({
+      _id:new monggose.Types.ObjectId(),
       name: req.body.name,
       price: req.body.price
-   }
+   })
+   product
+      .save()
+      .then(res => {
+         console.log(res)
+      })
+      .catch(err => console.log(err));
+
    res.status(201).json({
       message: 'Handling Post requests to /products',
       createdProduct: product
@@ -20,28 +41,55 @@ router.post('/', (req, res, next) => {
 
 router.get('/:productId', (req, res, next) => {
    const id = req.params.productId;
-   if(id === "spesial") {
-      res.status(200).json({
-         message: ' You discovered the spesial',
-         id:id
+   Product.findById(id)
+      .exec()
+      .then(doc => {
+         console.log("From database", doc);
+         if(doc) {
+            res.status(200).json(doc);
+         }else{
+            res.status(404).json({
+               message: 'No Invalid entry id'
+            })
+         }
       })
-   }else{
-      res.status(200).json({
-         message:'Not Discovered'
+      .catch(err => {
+         res.status(500).json({
+            message:err
+         })
       })
-   }
 })
 
-router.patch('/:productId', (req, res, next) => {
-   res.status(200).json({
-      message:'Updated success'
-   })
+router.put('/:productId', (req, res, next) => {
+   const id = req.params.productId;
+   Product.update({_id:id}, {name:req.body.name, price:req.body.price})
+      .exec()
+      .then(result => {
+         res.status(200).json({
+            result
+         })
+      })
+      .catch(err => {
+         res.status(500).json({
+            err
+         })
+      })
 })
 
 router.delete('/:productId', (req, res, next) => {
-   res.status(200).json({
-      message: 'Deleted success'
-   })
+   const id = req.params.productId;
+   Product.remove({_id:id})
+      .exec()
+      .then( result => {
+         res.status(200).json({
+            message:'Success'
+         })
+      })
+      .catch(err => {
+         res.status(500).json({
+            message:'delete data'
+         })
+      })
 })
 
 module.exports = router;
