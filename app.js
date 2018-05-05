7347d8151904fd11962f0920f55fe186d7c726f5
hariirawan <hari31121993.com>
2018-05-05 11:26:34 +0700

    Cors

--- app.js
+++ app.js
@@ -6,9 +6,21 @@ const bodyParser = require('body-parser');
 const productRoutes = require('./src/routes/products');
 const orderRoutes = require('./src/routes/orders');
 
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+
+app.use((res, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+   if(req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Headers','PUT,POST,PATCH,DELETE,GET');
+      return res.status(200).json({});
+   }
+   next();
+
+})
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
